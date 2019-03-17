b52fabdb4a7f243e37ead360f27e3bd4d07ac15b
unknown <jankkoziel@gmail.com>
2019-03-17 23:36:03 +0100

    Started working on callendar format

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,10 +3,20 @@ import Day from './Day'
 import { Months } from './Months'
 
 const Calendar = ({ date }) => {
-    const [weekList, setWeekList] = useState(new Array(6).fill(new Array(7)).fill(<Day />))
     const first = new Date(date.getFullYear(), date.getMonth(), 1);
+    
     const last = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    console.log(first, last)
+    let curMonth = new Array(6).fill(new Array(7).fill({}))
+    for (let week in curMonth) {
+        for (let day in curMonth[week]) {
+            console.log((parseInt(week) * 7 + parseInt(day)) % Months[date.getMonth()].days +1)
+            curMonth[week][day] = <Day day={(parseInt(week) * 7 + parseInt(day)) % Months[date.getMonth()].days +1} /> 
+        }
+    }
+    console.log(curMonth)
+    const [weekList, setWeekList] = useState()
+
+
     const today = {
         dayInMonth: date.getDate(),
         dayInWeek: date.getDay(),
